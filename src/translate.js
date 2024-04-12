fb015639dcba253a4b888c17dd4f60b68d908d96
raahul-k <raahulr.2805@gmail.com>
2024-04-12 11:00:05 +0530

    Fixing api key bug

--- src/translate.js
+++ src/translate.js
@@ -9,6 +9,7 @@ const GET_LANGS_URL =
   "https://translation.googleapis.com/language/translate/v2/languages";
 
 const translatedText = async (text, targetLanguage) => {
+  console.log("API KEY", API_KEY);
   const response = await axios.post(`${TRANSLATE_LANG_URL}?key=${API_KEY}`, {
     q: text,
     target: targetLanguage,
@@ -18,6 +19,7 @@ const translatedText = async (text, targetLanguage) => {
 };
 
 const detectedLang = async (text) => {
+  console.log("API KEY", API_KEY);
   const detectionResponse = await axios.post(
     `${DETECT_LANG_URL}?key=${API_KEY}`,
     { q: text }
@@ -27,6 +29,7 @@ const detectedLang = async (text) => {
 };
 
 const allLangs = async () => {
+  console.log("API KEY", API_KEY);
   const langs = await axios.post(`${GET_LANGS_URL}?target=en&key=${API_KEY}`);
 
   return langs.data.data.languages;
