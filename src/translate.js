ec104246fd22880f7d25e7fa0c9097820f51471c
raahul-k <raahulr.2805@gmail.com>
2024-04-12 11:01:34 +0530

    Final push

--- src/translate.js
+++ src/translate.js
@@ -9,7 +9,6 @@ const GET_LANGS_URL =
   "https://translation.googleapis.com/language/translate/v2/languages";
 
 const translatedText = async (text, targetLanguage) => {
-  console.log("API KEY", API_KEY);
   const response = await axios.post(`${TRANSLATE_LANG_URL}?key=${API_KEY}`, {
     q: text,
     target: targetLanguage,
@@ -19,7 +18,6 @@ const translatedText = async (text, targetLanguage) => {
 };
 
 const detectedLang = async (text) => {
-  console.log("API KEY", API_KEY);
   const detectionResponse = await axios.post(
     `${DETECT_LANG_URL}?key=${API_KEY}`,
     { q: text }
@@ -29,7 +27,6 @@ const detectedLang = async (text) => {
 };
 
 const allLangs = async () => {
-  console.log("API KEY", API_KEY);
   const langs = await axios.post(`${GET_LANGS_URL}?target=en&key=${API_KEY}`);
 
   return langs.data.data.languages;
